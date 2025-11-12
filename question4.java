abbfc44af0b93530a2fe0954d97636ea5b6d9609
Vishnu Dattu Lakkaraju <lvdattu2003@gmail.com>
2025-11-12 11:29:17 +0530

    Update question4.java

--- question4.java
+++ question4.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+public class Main {
 
+    public static void main(String[] args) {
+
+        //write your answer here
+         Scanner scanner = new Scanner(System.in);
+
+       
+        int num = scanner.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.print("No is Even");
+        } else {
+            System.out.print("No is odd");
+        }
+
+        scanner.close();
+        }
+}
